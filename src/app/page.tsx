94d7b575dc9e968e5f46e9c25735b3960499bc98
YUS / PIS <yusrilhafis@gmail.com>
2025-12-30 11:36:41 +0700

    feat: create gym visit and fitness chaser page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import GymVisitCard from "../components/GymVisitCard";
+import WeeklyChallengeCard from "../components/FitnessChaserCard";
 
 export default function Home() {
   return (
-    <>
-    <h1>AIA Vitality Wrapped</h1>
-    </>
-  )
+    <main
+      className="
+        h-[100svh]
+        overflow-y-scroll
+        snap-y
+        snap-mandatory
+      "
+    >
+      <section className="h-[100svh] snap-start snap-always">
+        <GymVisitCard />
+      </section>
+      <section className="h-[100svh] snap-start snap-always">
+        <GymVisitCard />
+      </section>
+
+      <section className="h-[100svh] snap-start snap-always">
+        <WeeklyChallengeCard />
+      </section>
+      <section className="h-[100svh] snap-start snap-always">
+        <WeeklyChallengeCard />
+      </section>
+    </main>
+  );
 }
---
+++ src/components/GymVisitCard.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import MobileCardFrame from "./MobileCardFrame";
+
+export default function GymVisitCard() {
+  const [counter] = useState(260);
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [background, setBackground] = useState("");
+
+  useEffect(() => {
+    if (counter <= 50) {
+      setTitle("Routines <br /> Starter");
+      setMessage("Nice Start!");
+      setBackground("linear-gradient(to bottom, #9393f9 35%, #d7f5ff 35%)");
+    } else if (counter <= 150) {
+      setTitle("Regular <br /> Powerlifter");
+      setMessage("Your habit <br /> is solid!");
+      setBackground(
+        "linear-gradient(to bottom, rgba(113,113,226,1) 35%, rgba(178,232,247,1) 35%)"
+      );
+    } else if (counter <= 250) {
+      setTitle("Strong <br /> Habit Builder");
+      setMessage("Love the <br /> discipline!");
+      setBackground(
+        "linear-gradient(to bottom, rgba(70,70,204,1) 35%, rgba(108,220,239,1) 35%)"
+      );
+    } else {
+      setTitle("Gym <br /> Crusher");
+      setMessage("You're <br /> unstoppable!");
+      setBackground(
+        "linear-gradient(to bottom, rgba(41,41,155,1) 35%, rgba(23,209,229,1) 35%)"
+      );
+    }
+  }, [counter]);
+
+  return (
+    <MobileCardFrame
+      background={background}
+      ornaments={
+        <div className="absolute inset-0 z-0">
+          <img
+            src="/gym-visit/triangle.svg"
+            className="absolute top-0 left-0 w-full h-[50%] object-cover opacity-50"
+          />
+          <img
+            src="/gym-visit/triangle.svg"
+            className="absolute bottom-0 left-0 w-full h-[50%] object-cover opacity-50"
+          />
+        </div>
+      }
+      illustration={
+        <img
+          src="/gym-visit/gym-visit-deadlift.svg"
+          className="absolute bottom-[40px] right-0 w-[250px] h-[300px] z-10"
+        />
+      }
+      topContent={
+        <h1
+          className="text-white text-[50px] font-bold leading-tight"
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+      }
+      bottomContent={
+        <>
+          <p className="text-[20px] text-black">You went to gym partner</p>
+          <h2 className="text-[50px] text-black font-bold leading-none">
+            {counter}
+          </h2>
+          <p className="text-[20px] font-medium text-black">times</p>
+          <p
+            className="mt-[30px] text-[15px] text-black"
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        </>
+      }
+    />
+  );
+}
