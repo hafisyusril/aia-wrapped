516c1c0685ce8c8945e8bc6458ead3ea5a6156c0
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2026-01-02 14:23:14 +0700

    fix: solving rank animation bug on production part 2

--- src/components/vitality-rank/VitalityRankCard.tsx
+++ src/components/vitality-rank/VitalityRankCard.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useInView } from "@/src/app/hooks/useInView";
 import { getVitalityRankTheme } from "./VitalityRankConfig";
 import RankCounter from "./RankCounter";
@@ -17,60 +16,46 @@ export default function VitalityRankCard({
     const theme = getVitalityRankTheme("default");
     const { ref, isInView } = useInView({ threshold: 0.6 });
 
-    const [shouldAnimate, setShouldAnimate] = useState(false);
-
-    useEffect(() => {
-        if (isInView && !shouldAnimate) {
-            setShouldAnimate(true);
-        }
-    }, [isInView, shouldAnimate]);
-
     return (
         <section
-            ref={ref}
             className={`w-full min-h-screen font-sans relative overflow-hidden ${theme.backgroundColor}`}
         >
-            <div className="absolute inset-0 flex justify-center items-center z-0 pointer-events-none">
+            <div className="absolute inset-0 z-0 pointer-events-none">
                 <img
                     src={theme.ornamentSrc}
+                    className="h-full opacity-80"
                     alt=""
-                    className="opacity-80 h-full"
                 />
             </div>
             <div
                 className={`absolute inset-y-0 right-0 w-[120px] opacity-60 ${theme.sideAccentColor}`}
             />
-            <div className="relative z-10 flex min-h-screen items-center px-6">
-                <div className="flex w-full flex-col py-12">
-                    <div className="mb-6 flex justify-between">
-                        <h1 className="text-4xl font-extrabold text-white">
-                            Vitality<br />Rank
-                        </h1>
 
-                        <img src={theme.trophySrc} className="w-[120px]" />
+            <div
+                ref={ref}
+                className="relative z-10 flex min-h-screen items-center px-6"
+            >
+                <div className="flex w-full flex-col py-12 gap-10">
+                    <div>
+                        <p className="text-white">General Rank</p>
+                        <p className="text-6xl font-extrabold text-white">
+                            {isInView ? (
+                                <RankCounter key="general" target={generalRank} />
+                            ) : (
+                                "#0"
+                            )}
+                        </p>
                     </div>
-                    <div className="mb-16 h-[2px] w-24 bg-white/70" />
-                    <div className="flex flex-col gap-10">
-                        <div>
-                            <p className="text-lg text-white">General Rank</p>
-                            <p className="text-6xl font-extrabold text-white">
-                                {shouldAnimate ? (
-                                    <RankCounter target={generalRank} />
-                                ) : (
-                                    "#0"
-                                )}
-                            </p>
-                        </div>
-                        <div>
-                            <p className="text-lg text-white">Gender Rank</p>
-                            <p className="text-6xl font-extrabold text-white">
-                                {shouldAnimate ? (
-                                    <RankCounter target={genderRank} />
-                                ) : (
-                                    "#0"
-                                )}
-                            </p>
-                        </div>
+
+                    <div>
+                        <p className="text-white">Gender Rank</p>
+                        <p className="text-6xl font-extrabold text-white">
+                            {isInView ? (
+                                <RankCounter key="gender" target={genderRank} />
+                            ) : (
+                                "#0"
+                            )}
+                        </p>
                     </div>
                 </div>
             </div>
