5a7cc42b380b1e7b80ff8c13647cc9b47a114176
YUS / PIS <yusrilhafis@gmail.com>
2026-01-26 10:38:57 +0700

    fix: share button

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ShareIcon from "./ShareIcon";
 
 type ShareButtonProps = {
   onClick?: () => void;
@@ -6,11 +7,14 @@ type ShareButtonProps = {
   isBrightBg?: boolean; // true = background terang → hitam, false = gelap → putih
 };
 
+
 export default function ShareButton({
   onClick,
   className,
   isBrightBg = false,
 }: ShareButtonProps) {
+  const iconColor = isBrightBg ? "#000" : "#fff";
+
   return (
     <button
       onClick={onClick}
@@ -18,8 +22,7 @@ export default function ShareButton({
         absolute top-4 right-4 z-30
         flex items-center gap-2
         rounded-full
-        
-         ${isBrightBg ? "bg-[#989898]/20" : "bg-white/20"}
+        ${isBrightBg ? "bg-[#989898]/20" : "bg-white/20"}
         px-4 py-2
         text-sm font-normal
         hover:opacity-90
@@ -27,23 +30,11 @@ export default function ShareButton({
         transition
         ${className ?? ""}
       `}
-      style={{
-        color: isBrightBg ? "#000" : "#fff", // text juga mengikuti
-      }}
+      style={{ color: iconColor }}
     >
-      <svg
-        width="15"
-        height="15"
-        viewBox="0 0 45 45"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M43.7 0.280504C43.9 0.480504 44 0.680502 44 0.880502C44.1 1.1805 44.2 1.4805 44.1 1.7805C44.1 1.8805 30.6 44.3805 30.6 44.3805L18 25.8805L0 13.5805L42.4 0.080507C42.9 -0.119493 43.4 0.0805024 43.8 0.380502L43.7 0.280504ZM40.4 5.3805L20.5 25.2805L29.7 38.7805L40.3 5.3805H40.4ZM38.8 3.58051L5.39996 14.1805L18.9 23.3805L38.8 3.48051V3.58051Z"
-          fill={isBrightBg ? "#000" : "#fff"} // icon juga mengikuti
-        />
-      </svg>
-      <span>Share</span>
+      <ShareIcon color={iconColor} />
+      <span>Save and Share</span>
     </button>
   );
 }
+
