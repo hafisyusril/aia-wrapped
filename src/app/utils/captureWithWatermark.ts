4ed6386ff827b28cca62848217b5bab7e9fcce52
YUS / PIS <yusrilhafis@gmail.com>
2026-02-07 10:00:35 +0700

    fix: font and ojk placement back to 20px

--- src/app/utils/captureWithWatermark.ts
+++ src/app/utils/captureWithWatermark.ts
@@ -191,7 +191,7 @@ export async function captureWithWatermarkV2({
       "PT AIA Financial berizin dan diawasi oleh Otoritas Jasa Keuangan";
 
     const textOffsetX = 40;
-    const textOffsetY = 100; // Khusus Page crowning dibuat lebih tinggi pageName === "Crowning" ?
+    const textOffsetY = pageName === "Crowning" ? 100 : 20; // Khusus Page crowning dibuat lebih tinggi pageName === "Crowning" ?
 
     const textPaddingX = canvas.width - padding - textOffsetX;
     const textPaddingY = padding + textOffsetY;
